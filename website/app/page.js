5337482c8d50c0cdb055c20c5666f75ee800641c
Harry J. <harryjiang0114@gmail.com>
2024-09-13 02:57:54 -0400

    suspense???

--- website/app/page.js
+++ website/app/page.js
@@ -8,31 +8,31 @@ export default function Home() {
 
   return (
     <main>
-      <section class="section has-text-centered">
-        <h1 class="title">Audio Dataset Search</h1>
-        <h2 class="subtitle">
-          Search for keywords for works that may appear in audio ML training datasets. Examples include a work's title or the name of the creator, a performer, or the rights owner.
-        </h2>
+      <Suspense>
+        <section class="section has-text-centered">
+          <h1 class="title">Audio Dataset Search</h1>
+          <h2 class="subtitle">
+            Search for keywords for works that may appear in audio ML training datasets. Examples include a work's title or the name of the creator, a performer, or the rights owner.
+          </h2>
 
-        <div class="container is-max-desktop">
-          <div class="field is-grouped">
-            <Suspense>
+          <div class="container is-max-desktop">
+            <div class="field is-grouped">
               <input class="input is-rounded" type="text" placeholder="Search" />
-            </Suspense>
-            <p class="buttons">
-              <button class="button is-rounded">
-                <span class="icon is-link">
-                  <i class="fas fa-magnifying-glass"></i>
-                </span>
-              </button>
-            </p>
+              <p class="buttons">
+                <button class="button is-rounded">
+                  <span class="icon is-link">
+                    <i class="fas fa-magnifying-glass"></i>
+                  </span>
+                </button>
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div class="container is-max-desktop">
-          <p>Search: {search}</p>
-        </div>
-      </section>
+          <div class="container is-max-desktop">
+            <p>Search: {search}</p>
+          </div>
+        </section>
+      </Suspense>
     </main>
   );
 }
