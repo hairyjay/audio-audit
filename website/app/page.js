b761903b31d25b87d6cbbbcada56182fafacc7fd
Harry J. <harryjiang0114@gmail.com>
2024-09-13 03:16:52 -0400

    search

--- website/app/page.js
+++ website/app/page.js
@@ -6,50 +6,42 @@ export default function Home() {
   return (
     <main>
       <section class="section has-text-centered">
-        <h1 class="title">Audio Dataset Search</h1>
-        <h2 class="subtitle">
-          Search for keywords for works that may appear in audio ML training datasets. Examples include a work's title or the name of the creator, a performer, or the rights owner.
-        </h2>
-
         <div class="container is-max-desktop">
-          <div class="field is-grouped">
-            <Suspense>
-              <Searchbar />
-            </Suspense>
-            <p class="buttons">
-              <button class="button is-rounded">
-                <span class="icon is-link">
-                  <i class="fas fa-magnifying-glass"></i>
-                </span>
-              </button>
-            </p>
-          </div>
+          <h1 class="title">Audio Dataset Search</h1>
+          <h2 class="subtitle">
+            Search for keywords for works that may appear in audio ML training datasets. Examples include a work's title or the name of the creator, a performer, or the rights owner.
+          </h2>
         </div>
 
         <Suspense>
-          <Results />
+          <Search />
         </Suspense>
       </section>
     </main>
   );
 }
 
-function Searchbar() {
+function Search() {
   const searchParams = useSearchParams()
   const search = searchParams.get('q')
 
   return (
-    <input class="input is-rounded" type="text" placeholder="Search" value={search} />
-  );
-}
-
-function Results() {
-  const searchParams = useSearchParams()
-  const search = searchParams.get('q')
-
-  return (
-    <div class="container is-max-desktop">
-      <p>Search: {search}</p>
+    <div>
+      <div class="container is-max-desktop">
+        <div class="field is-grouped">
+          <input class="input is-rounded" type="text" placeholder="Search" value={search} />
+          <p class="buttons">
+            <button class="button is-rounded">
+              <span class="icon is-link">
+                <i class="fas fa-magnifying-glass"></i>
+              </span>
+            </button>
+          </p>
+        </div>
+      </div>
+      <div class="container is-max-desktop">
+        <p>Search: {search}</p>
+      </div>
     </div>
   );
 }
