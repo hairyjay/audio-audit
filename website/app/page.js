d028a3a1935181313a935cc837f1c2b31db2583c
Harry J. <harryjiang0114@gmail.com>
2024-09-13 03:32:41 -0400

    sections

--- website/app/page.js
+++ website/app/page.js
@@ -5,18 +5,9 @@ import { Suspense } from 'react'
 export default function Home() {
   return (
     <main>
-      <section class="section has-text-centered">
-        <div class="container is-max-desktop">
-          <h1 class="title">Audio Dataset Search</h1>
-          <h2 class="subtitle">
-            Search for keywords for works that may appear in audio ML training datasets.<br></br>Examples include a work's title or the name of the creator, a performer, or the rights owner.
-          </h2>
-        </div>
-
         <Suspense>
           <Search />
         </Suspense>
-      </section>
     </main>
   );
 }
@@ -27,20 +18,31 @@ function Search() {
 
   return (
     <div>
-      <div class="container is-max-desktop">
-        <div class="field is-grouped">
-          <input class="input is-rounded" type="search" placeholder="Search" value={search} />
-          <p class="buttons">
-            <button class="button is-rounded is-link">
-              Search
-            </button>
-          </p>
+      <section class="section has-text-centered">
+        <div class="container is-max-desktop">
+          <h1 class="title">Audio Dataset Search</h1>
+          <h2 class="subtitle">
+            Search for keywords for works that may appear in audio ML training datasets.<br></br>Examples include a work's title or the name of the creator, a performer, or the rights owner.
+          </h2>
         </div>
-      </div>
 
-      <div class="container is-max-desktop">
-        <p>Search: {search}</p>
-      </div>
+        <div class="container is-max-desktop">
+          <div class="field is-grouped">
+            <input class="input is-rounded" type="search" placeholder="Search" value={search} />
+            <p class="buttons">
+              <button class="button is-rounded is-link">
+                Search
+              </button>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section class="section has-text-centered">
+        <div class="container is-max-desktop">
+          <p>Search: {search}</p>
+        </div>
+      </section>
     </div>
   );
 }
