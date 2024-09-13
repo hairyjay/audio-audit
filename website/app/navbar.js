3595d94620c31716e844a39fe233f2eca03a278c
Harry J. <harryjiang0114@gmail.com>
2024-09-13 01:41:51 -0400

    typo

--- website/app/navbar.js
+++ website/app/navbar.js
@@ -1,32 +1,32 @@
 export default function Navbar() {
   return (
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
-            Audio Dataset Audit
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          Audio Dataset Audit
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            Search
           </a>
 
-          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+          <a class="navbar-item">
+            About
           </a>
-        </div>
-
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">
-              Search
-            </a>
-
-            <a class="navbar-item">
-              About
-            </a>
-
           <div class="navbar-end">
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
